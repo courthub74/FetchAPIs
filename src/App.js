37ec7a1ba39c30fab390381f6bdcac67f0aa89d0
courthub74 <courdevelops@gmail.com>
2022-01-05 19:32:12 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,15 +3,53 @@ import './style.css';
 
 import { useState, useEffect } from 'react';
 
-export default function App() {
-  const { resourceType, setResourceType } = useState('todos');
-  const { items, setItems } = useState([]);
-  const { total, setTotal } = useState(0);
+const App = () => {
+  const [resourceType, setResourceType] = useState('Todos');
+  const [items, setItems] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setItems(json);
+        setTotal(json.length);
+      });
+  });
+
   return (
     <>
-      <button onClick={null}>ToDos</button>
-      <button onClick={null}>Comments</button>
-      <button onClick={null}>Posts</button>
+      <button
+        onClick={() => {
+          setResourceType('Todos');
+        }}
+      >
+        ToDos
+      </button>
+      <button
+        onClick={() => {
+          setResourceType('Comments');
+        }}
+      >
+        Comments
+      </button>
+      <button
+        onClick={() => {
+          setResourceType('Posts');
+        }}
+      >
+        Posts
+      </button>
+
+      <h1>{total}</h1>
+      <h2>{resourceType}</h2>
+      <div>
+        {items.map((item) => {
+          return <p>{JSON.stringify(item)}</p>;
+        })}
+      </div>
     </>
   );
-}
+};
+
+export default App;
